7fa44c1709f3580c6effc0a99895bfad37fccdec
Jack Camas <jackcamasdev@gmail.com>
2023-08-25 03:33:31 -0400

    Initial commit

--- CodePath-Prework/ViewController.swift
+++ CodePath-Prework/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
+	func changeColor() -> UIColor {
+		let red = CGFloat.random(in: 0...1)
+		let green = CGFloat.random(in: 0...1)
+		let blue = CGFloat.random(in: 0...1)
+		return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+	}
 
+	@IBAction func changeBackgroundColor(_ sender: Any) {
+		let randomColor = changeColor()
+		view.backgroundColor = randomColor
+	}
+	
 }
 
